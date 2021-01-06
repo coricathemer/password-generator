7d3360ae03596c02698685db7cee308234d841db
Cori Cathemer <coricathemer@gmail.com>
2021-01-06 14:28:32 -0700

    updated js

--- Develop/script.js
+++ Develop/script.js
@@ -3,25 +3,45 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  console.log("Hello World")
-  // prompts user to choose a numerical length between 8 and 128
-  var userPasswordLength = prompt("Please choose a password in length from 8 to 128 characters.")
-  console.log(userPasswordLength)
-  // prompts user to select password characters
-  var userPasswordChar = prompt("Please choose a lowercase, UPPERCASE, numeric, and/or special character.")
-  console.log(userPasswordChar)
-
-}
-
-function generatePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
-
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// write the password here
+
+function generatePassword() {
+  var length = prompt("Password must be between 8 and 128 characters.")
+  if (length > 8 || length < 128) {
+    window.alert("Great!")
+  } else {
+    window.alert("Please select a length between 8 and 128")
+  }
+  var lower = prompt("Please Select a lower case letter between a-z.")
+  if (lower === "abcdefghijklmnopqrstuvwxyz") {
+    window.alert("Fantastic!")
+  } else {
+    window.alert("Please select a lower case letter between a-z")
+  }
+  var upper = prompt("Please select an UPPER case letter between A-Z")
+  if (upper === "ABCDEFGHIJKLMNOPQRSTUVWXYZ") {
+    window.alert("Super!")
+  } else {
+    window.alert("Please select an UPPER case letter between A-Z")
+  }
+  var number = prompt("Please pick a number between 0-9")
+  if (number > 9) {
+    window.alert("Wonderful!")
+  } else {
+    window.alert("Please pick a number between 0-9")
+  }
+  var symbol = prompt("Please pick a special character !@#$%^&(.")
+  if (symbol === true) {
+    window.alert("Marvelous!")
+  } else {
+    window.alert("Please pick a special character !@#$%^&*(.")
+  }
+}

13f9c6802ea81ab2cccec9ebfab202f35694e1b3
coricathemer <coricathemer@gmail.com>
2021-01-06 22:28:18 -0700

    final commit

--- Develop/script.js
+++ Develop/script.js
@@ -14,70 +14,54 @@ generateBtn.addEventListener("click", writePassword);
 // when a user is prompted to input what charachters they would like to use, the data is stored and then randomized into a unique password 
 
 function generatePassword() {
-  var length = prompt("Password must be between 8 and 128 characters.")
-  var lenghtChars = "8-128"
-  var confirmLengthChars = confirm("Would you like to confirm selection?")
-  var userChoiceCrit = [];
-  if (confirmLengthChars === true) {
-    userChoiceCrit.push(confirmLengthChars)
+  var length = parseInt(prompt("Password must be between 8 and 128 characters."))
+  var confirmLower = confirm("Do you want lower case letters?")
+  var confirmUpper = confirm("Do you want UPPER case letters?")
+  var confirmNumber = confirm("Do you want a number?")
+  var confirmSymbol = confirm("Do you want to include special characters?")
+
+  if (length <128 || length >8) {
+
+  } else {
+    window.alert("Password must be between 8 and 128 characters.")
   }
-  // if (length < 8 || length > 128) {
-  //   window.alert("Great!")
-  // } else {
-  //   window.alert("Please select a length between 8 and 128")
-  // }
-  var lower = prompt("Please Select a lower case letter between a-z.")
   var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  var confirmLowerChars = confirm("Would you like to confirm selection?")
-  var userChoiceCrit = [];
-  if (confirmLowerChars === true) {
-    userChoiceCrit.push(confirmLowerChars)
-  }
-  // const alphabet = [...'abcdefghijklmnopqrstuvwxyz']
-  // if (lower === "abcdefghijklmnopqrstuvwxyz") {
-  //   window.alert("Fantastic!")
-  // } else {
-  //   window.alert("Please select a lower case letter between a-z")
-  // }
-  var upper = prompt("Please select an UPPER case letter between A-Z")
   var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  var confirmUpperChars = confirm("Would you like to confirm selection?")
-  var userChoiceCrit = [];
-  if (confirmUpperChars === true) {
-    userChoiceCrit.push(confirmUpperChars)
+  var numberChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbolChars = ["!", "@", "#", "$", "%", "^", "&", "*", "("]
+
+  var possibleChars = [];
+
+  if (confirmLower){
+    for(let i= 0; i<lowerChars.length; i++){
+      possibleChars.push( lowerChars[i] )
+    }
   }
-  // if (upper === "ABCDEFGHIJKLMNOPQRSTUVWXYZ") {
-  //   window.alert("Super!")
-  // } else {
-  //   window.alert("Please select an UPPER case letter between A-Z")
-  // }
-  var number = prompt("Please pick a number between 0-9")
-  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  var confirmNumbers = confirm("Would you like to confirm selection?")
-  var userChoiceCrit = [];
-  if (confirmNumbers === true) {
-    userChoiceCrit.push(confirmNumbers)
-  }  
-  // if (number > 9) {
-  //   window.alert("Wonderful!")
-  // } else {
-  //   window.alert("Please pick a number between 0-9")
-  // }
-  var symbol = prompt("Please pick a special character !@#$%^&(.")
-  var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "("]
-  var confirmSymbols = confirm("Would you like to confirm selection?")
-  var userChoiceCrit = []; 
-  if (confirmSymbols === true) {
-    userChoiceCrit.push(confirmSymbols)
+  if (confirmUpper){
+    for(let i= 0; i<upperChars.length; i++){
+      possibleChars.push( upperChars[i] )
+    }
   }
-  // if (symbol === true) {
-  //   window.alert("Marvelous!")
-  // } else {
-  //   window.alert("Please pick a special character !@#$%^&*(.")
-  // }
-  var passwordArr = ["length", "lowerChars", "upperChars", "numbers", "symbols"]
-  console.log(passwordArr[0])
-  for (var i=0)
-  passwordArr.length
-  for (var i=0; i<passwordArr.length)
+  if (confirmNumber){
+    for(let i= 0; i<numberChars.length; i++){
+      possibleChars.push( numberChars[i] )
+    }
+  }
+  if (confirmSymbol){
+    for(let i= 0; i<symbolChars.length; i++){
+      possibleChars.push( symbolChars[i] )
+    }
+  }
+  console.log(possibleChars)
+  //start building a string.
+  var finalPassword = "";
+  // something with the length
+  for(let i= 0; i<length; i++){
+    var random = Math.floor(Math.random()* possibleChars.length)
+    // random char from possibleChars
+    finalPassword = finalPassword + possibleChars[random];
+  }
+  return finalPassword;
+
+ 
 }
